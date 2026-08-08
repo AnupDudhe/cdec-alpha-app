981539a6db3af0ee9debda6ac386ff5504c50bf4
Anup Dudhe <152959908+AnupDudhe@users.noreply.github.com>
2026-08-08 14:47:53 +0530

    Change S3 bucket for Terraform backend

--- infrastructure/frontend/backend.tf
+++ infrastructure/frontend/backend.tf
@@ -6,7 +6,7 @@
 
 terraform {
   backend "s3" {
-    bucket  = "cdec-alpha-terraform-state-atulyw"
+    bucket  = "cdec-alpha-terraform-state-anup"
     key     = "frontend/terraform.tfstate"
     region  = "eu-west-1"
     encrypt = true
